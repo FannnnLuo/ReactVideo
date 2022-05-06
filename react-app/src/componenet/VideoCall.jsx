c05f642d5fd95888c6af0c62defec7445a23f583
ppm10103 <ppm10103@163.com>
2022-05-06 19:07:09 -0400

    wss error issues

--- react-app/src/componenet/VideoCall.jsx
+++ react-app/src/componenet/VideoCall.jsx
@@ -1,37 +1,60 @@
 import React from 'react'
-import { Button,TextField } from '@mui/material';
-import { useRef ,useState,useEffect} from 'react';
-import io from 'socket.io-client'
+import {Button, TextField} from '@mui/material';
+import {useRef, useState, useEffect} from 'react';
+import io, {Manager} from 'socket.io-client'
 import Peer from "simple-peer"
 
 
-const socket = io.connect('https://localhost:8000')
+// const socket = io('https://10.0.0.45:8000', {
+//     path: '/socket.io/',
+//     transports: ["websocket"]
+// })
+// socket.on("connect_error", (err) => {
+//     // revert to classic upgrade
+//     console.log(err)
+// });
+//
+
+const manager = new Manager('wss://10.0.0.45:8000', {rejectUnauthorized: false, transports: ["websocket"]})
+const socket = manager.socket('/')
+
+manager.on('error', (error) => {
+    // Fired upon a connection error.
+    console.log('socket error', error)
+})
+
+socket.on('connect_error', (error) => {
+    // Fired when an namespace middleware error occurs.
+    console.log('connection error', error)
+})
 
 export default function VideoCall() {
     const myVideo = useRef()
     const calleeVideo = useRef()
-    const connectionRef= useRef()
+    const connectionRef = useRef()
 
     const [stream, setStream] = useState(null)
     const [me, setMe] = useState('')
     const [callerSignal, setCallerSignal] = useState(null)
     const [caller, setCaller] = useState(null)
-    const [callee,setCallee] = useState('')
+    const [callee, setCallee] = useState('')
 
 
     useEffect(() => {
-        socket.on('me', id => { setMe(id) })
+        socket.on('me', id => {
+            setMe(id)
+        })
 
         // io.to(callee).emit('call',{signal,caller})
-        socket.on('call', ({ signal, caller }) => {
+        socket.on('call', ({signal, caller}) => {
             setCaller(caller)
             setCallerSignal(signal)
             console.log(`caller:${caller}, signal:${signal}`)
         })
     }, [])
-    
+
     const clickHandler = () => {
-        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        navigator.mediaDevices.getUserMedia({video: true, audio: true})
             .then(stream => {
                 setStream(stream)
                 myVideo.current.srcObject = stream
@@ -40,17 +63,19 @@ export default function VideoCall() {
 
     const callUser = (id) => {
         const peer = new Peer({
-			initiator: true,
-			trickle: false,
-			stream: stream
+            initiator: true,
+            trickle: false,
+            stream: stream
         })
         peer.on('signal', signal => {
-            socket.emit('call',{caller:me,callee:id,signal})
+            socket.emit('call', {caller: me, callee: id, signal})
         })
         peer.on('stream', stream => {
             calleeVideo.current.srcObject = stream
         })
-        socket.on('accepted', signal => { peer.signal(signal) })
+        socket.on('accepted', signal => {
+            peer.signal(signal)
+        })
         connectionRef.current = peer
     }
     const callClickHandler = () => {
@@ -58,39 +83,41 @@ export default function VideoCall() {
     }
     const answerClickHandler = () => {
         const peer = new Peer({
-			initiator: false,
-			trickle: false,
-			stream: stream
+            initiator: false,
+            trickle: false,
+            stream: stream
         })
         peer.on('signal', signal => {
-            socket.emit('answer',{signal,caller})
+            socket.emit('answer', {signal, caller})
         })
         peer.on('stream', stream => {
             calleeVideo.current.srcObject = stream
         })
         peer.signal(callerSignal)
         connectionRef.current = peer
-        
+
     }
 
     return (
         <div>
             <Button variant='outlined' onClick={clickHandler}>Start My Video</Button><br/><br/>
-            <span>{ `${caller} is calling`}</span>
+            <span>{`${caller} is calling`}</span>
             <Button variant='outlined' onClick={answerClickHandler}>Answer</Button>
             <div>
                 <h3>My video</h3>
-                <h5>My id:{ me || 'error'}</h5>
-                <video playsInline autoPlay muted ref={myVideo}></video><br/>
-                
-                <TextField label='id to call' variant='standard' value={callee} onChange={e => setCallee(e.target.value)}></TextField>
+                <h5>My id:{me || 'error'}</h5>
+                <video playsInline autoPlay muted ref={myVideo}></video>
+                <br/>
+
+                <TextField label='id to call' variant='standard' value={callee}
+                           onChange={e => setCallee(e.target.value)}></TextField>
                 <Button variant='outlined' onClick={callClickHandler}>Call</Button>
             </div>
             <hr/>
             <div>
                 <h3>Callee's video</h3>
-                <video playsInline autoPlay ref={ calleeVideo}></video>
-            </div>  
+                <video playsInline autoPlay ref={calleeVideo}></video>
+            </div>
         </div>
     )
 }
